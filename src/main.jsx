916fbbacf21f7ced504feee8259ac4f83ca019bf
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-07-23 15:25:56 +0600

    Add create redux store and connect to app with Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 import './styles/index.css'
-import App from './app/App.jsx'
+import { App, store } from './app'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 )
