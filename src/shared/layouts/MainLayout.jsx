452af75753c5b9cb2ba0f0c0c071e13468b690e6
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-07-24 12:55:20 +0600

    Fix scoll-to-top issue on navigating to new page

--- src/shared/layouts/MainLayout.jsx
+++ src/shared/layouts/MainLayout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router'
 import NavBar from '@/shared/components/NavBar'
+import ScrollToTop from '../components/ScrollToTop'
 
 const MainLayout = () => {
   return (
     <>
+      <ScrollToTop />
       <NavBar />
-      <Outlet />
+      <main>
+        <Outlet />
+      </main>
     </>
   )
 }
