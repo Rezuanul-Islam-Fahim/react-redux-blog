d0c554616cb1ff3c336a0ae72f065d9c8ef86c0e
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-07-24 20:24:53 +0600

    Show author name on post-detail page

--- src/features/posts/pages/PostDetailPage.jsx
+++ src/features/posts/pages/PostDetailPage.jsx
@@ -1,6 +1,7 @@
 import { Link, useParams } from 'react-router'
 import { useSelector } from 'react-redux'
 import { selectPostById } from '../postsSlice'
+import PostAuthor from '../components/PostAuthor'
 
 const PostDetailPage = () => {
   const { id } = useParams()
@@ -9,8 +10,11 @@ const PostDetailPage = () => {
   return (
     <div className="container mx-auto py-10">
       <div className="border border-gray-300 max-w-3xl mx-auto border-solid p-5 rounded-lg my-4">
-        <h4 className="text-2xl font-semibold mb-3">{post.title}</h4>
-        <p>{post.content}</p>
+        <h4 className="text-2xl font-semibold">{post.title}</h4>
+        <div className="flex flex-row mb-4">
+          <PostAuthor userId={post.userId} />
+        </div>
+        <p className="text-lg">{post.content}</p>
         <Link to={`/edit-post/${id}`} className="btn btn-neutral mt-5">
           Edit Post
         </Link>
