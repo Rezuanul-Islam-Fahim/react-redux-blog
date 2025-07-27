50e47ab2ec3704d723f9c820d7b9ee8077252af7
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-07-28 00:32:00 +0600

    Implement loader and post-load feature

--- src/features/posts/components/PostList.jsx
+++ src/features/posts/components/PostList.jsx
@@ -1,16 +1,24 @@
-import { useDispatch, useSelector } from 'react-redux'
-import { Link } from 'react-router'
-import { fetchPosts, selectAllPosts, selectPostsStatus } from '../postsSlice'
-import PostAuthor from './PostAuthor'
-import TimeAgo from '@/features/posts/components/TimeAgo'
-import ReactionButtons from './ReactionButtons'
 import { useEffect } from 'react'
+import { Link } from 'react-router'
+import { useDispatch, useSelector } from 'react-redux'
+import { LoaderIcon } from 'lucide-react'
+import TimeAgo from '@/features/posts/components/TimeAgo'
+import PostAuthor from './PostAuthor'
+import ReactionButtons from './ReactionButtons'
+import {
+  fetchPosts,
+  selectAllPosts,
+  selectPostsError,
+  selectPostsStatus,
+} from '../postsSlice'
 
 const PostList = () => {
   const dispatch = useDispatch()
   const postsStatus = useSelector(selectPostsStatus)
+  const postsError = useSelector(selectPostsError)
   const posts = useSelector(selectAllPosts)
   const sortedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+  let content = null
 
   useEffect(() => {
     if (postsStatus === 'idle') {
@@ -24,28 +32,40 @@ const PostList = () => {
     return subStr.length < s.length ? subStr + '...' : s
   }
 
-  return (
-    <>
-      <h3 className="text-4xl font-semibold mb-3 mt-10">Posts</h3>
-      {sortedPosts.map(post => (
-        <div
-          key={post.id}
-          className="border border-gray-300  border-solid p-5 rounded-lg my-4"
-        >
-          <Link to={`/posts/${post.id}`}>
-            <h4 className="text-2xl font-semibold link link-primary link-hover">
-              {post.title}
-            </h4>
-          </Link>
-          <div className="flex flex-row mb-4">
-            <PostAuthor userId={post.userId} />
-            <TimeAgo timestamp={post.date} />
+  if (postsStatus === 'pending') {
+    content = <LoaderIcon className="animate-spin size-10 mx-auto my-10" />
+  } else if (postsStatus === 'succeeded') {
+    content = (
+      <>
+        {sortedPosts.map(post => (
+          <div
+            key={post.id}
+            className="border border-gray-300  border-solid p-5 rounded-lg my-4"
+          >
+            <Link to={`/posts/${post.id}`}>
+              <h4 className="text-2xl font-semibold link link-primary link-hover">
+                {post.title}
+              </h4>
+            </Link>
+            <div className="flex flex-row mb-4">
+              <PostAuthor userId={post.userId} />
+              <TimeAgo timestamp={post.date} />
+            </div>
+            <p className="text-lg">{getPostContentStr(post.content)}</p>
+            <ReactionButtons postId={post.id} />
           </div>
-          <p className="text-lg">{getPostContentStr(post.content)}</p>
-          <ReactionButtons postId={post.id} />
-        </div>
-      ))}
-    </>
+        ))}
+      </>
+    )
+  } else if (postsStatus === 'failed') {
+    content = <p>{postsError}</p>
+  }
+
+  return (
+    <section>
+      <h3 className="text-4xl font-semibold mb-3 mt-10">Posts</h3>
+      {content}
+    </section>
   )
 }
 
