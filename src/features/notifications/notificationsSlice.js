1bab7c58a301ead07378cfc7b4757f3813694ac6
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-07-31 22:54:27 +0600

    Feature: implement async fetchNotifications thunk and update notifications slice

--- src/features/notifications/notificationsSlice.js
+++ src/features/notifications/notificationsSlice.js
@@ -1,10 +1,31 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { client } from '@/api/client'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
 const initialNotifications = []
+
+const fetchNotifications = createAsyncThunk(
+  'notifications/fetchNotifications',
+  async (_, thunkApi) => {
+    const allNotifications = selectAllNotifications(thunkApi.getState())
+    const [latestNotification] = allNotifications
+    const latestTimestamp = latestNotification ? latestNotification.date : ''
+    const response = await client.get(
+      '/fakeApi/notifications?since=' + latestTimestamp
+    )
+    return response.data
+  }
+)
 
 const notificationsSlice = createSlice({
   name: 'notifications',
   initialState: initialNotifications,
+  reducers: {},
+  extraReducers: builder => {
+    builder.addCase(fetchNotifications.fulfilled, (state, action) => {
+      state.push(...action.payload)
+      state.sort((a, b) => b.date.localeCompare(a.date))
+    })
+  },
 })
 
 export const notificationsReducer = notificationsSlice.reducer
