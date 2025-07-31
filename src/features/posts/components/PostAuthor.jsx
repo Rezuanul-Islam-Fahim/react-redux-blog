a52b57e2cff4470acd9642e6fd489eb5c2fd2d6c
Rezuanul Islam Fahim <rifahim98@gmail.com>
2025-08-01 00:06:37 +0600

    Feature: add showPrefix prop to PostAuthor component for customizable author display

--- src/features/posts/components/PostAuthor.jsx
+++ src/features/posts/components/PostAuthor.jsx
@@ -1,10 +1,15 @@
 import { useSelector } from 'react-redux'
 import { fetchUserById } from '@/features/users'
 
-const PostAuthor = ({ userId }) => {
+const PostAuthor = ({ userId, showPrefix = true }) => {
   const user = useSelector(state => fetchUserById(state, userId))
 
-  return <span>By {user?.name ?? 'Undefined'}</span>
+  return (
+    <span>
+      {showPrefix ? 'by ' : null}
+      {user?.name ?? 'Undefined'}
+    </span>
+  )
 }
 
 export default PostAuthor
